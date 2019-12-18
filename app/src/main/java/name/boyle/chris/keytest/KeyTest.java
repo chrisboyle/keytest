6b95f34a1600644b8a209861c82c0ca19d7f6a59
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2019-12-18 10:07:24 +0000

    Trace touch events as well Some reformatting

--- app/src/main/java/name/boyle/chris/keytest/KeyTest.java
+++ app/src/main/java/name/boyle/chris/keytest/KeyTest.java
@@ -11,72 +11,72 @@ import android.widget.TextView;
 
 public class KeyTest extends Activity implements InputManager.InputDeviceListener {
 
-	private InputManager mInputManager;
-	TextView text;
+    private InputManager mInputManager;
+    TextView text;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.main);
-		text = (TextView) findViewById(R.id.text);
+        setContentView(R.layout.main);
+        text = (TextView) findViewById(R.id.text);
 
-		mInputManager = (InputManager)getSystemService(Context.INPUT_SERVICE);
-	}
+        mInputManager = (InputManager)getSystemService(Context.INPUT_SERVICE);
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		writeLog("onResume");
-		mInputManager.registerInputDeviceListener(this, null);
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        writeLog("onResume");
+        mInputManager.registerInputDeviceListener(this, null);
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		writeLog("onPause");
-		mInputManager.unregisterInputDeviceListener(this);
-	}
+    @Override
+    protected void onPause() {
+        super.onPause();
+        writeLog("onPause");
+        mInputManager.unregisterInputDeviceListener(this);
+    }
 
-	@Override
-	public boolean dispatchKeyEvent(final KeyEvent event) {
-		writeLog("dispatchKeyEvent: " + event.toString());
-		return false;
-	}
+    @Override
+    public boolean dispatchKeyEvent(final KeyEvent event) {
+        writeLog("dispatchKeyEvent: " + event.toString());
+        return false;
+    }
 
-	@Override
-	public boolean onGenericMotionEvent(MotionEvent event) {
-		writeLog("onGenericMotionEvent: " + event.toString());
-		return super.onGenericMotionEvent(event);
-	}
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        writeLog("onGenericMotionEvent: " + event.toString());
+        return super.onGenericMotionEvent(event);
+    }
 
-	@Override
-	public void onInputDeviceAdded(int i) {
-		writeLog("onInputDeviceAdded: " + mInputManager.getInputDevice(i));
-	}
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        writeLog("onTouchEvent: " + event.toString());
+        return super.onTouchEvent(event);
+    }
 
-	@Override
-	public void onInputDeviceRemoved(int i) {
-		writeLog("onInputDeviceRemoved: " + i);
-	}
+    @Override
+    public void onInputDeviceAdded(int i) {
+        writeLog("onInputDeviceAdded: " + i + "\n" + mInputManager.getInputDevice(i));
+    }
 
-	@Override
-	public void onInputDeviceChanged(int i) {
-		writeLog("onInputDeviceChanged: " + i);
-	}
+    @Override
+    public void onInputDeviceRemoved(int i) {
+        writeLog("onInputDeviceRemoved: " + i);
+    }
 
-	@Override
-	public void onBackPressed() {
-		writeLog("onBackPressed");
-		// Suppress back button behaviour by not calling overridden method
-	}
+    @Override
+    public void onInputDeviceChanged(int i) {
+        writeLog("onInputDeviceChanged: " + i);
+    }
 
-	private void writeLog(String logEvent) {
-		Log.i("KeyTest", logEvent);
-		if(text != null) {
-			text.setText(logEvent + "\n" + text.getText());
-		}
-	}
+    private void writeLog(String logEvent) {
+        Log.i("KeyTest", logEvent);
+        if(text != null) {
+            text.setText(logEvent + "\n" + text.getText());
+        }
+    }
 }
 
 
