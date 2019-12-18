d737b2e979cc261fbdacafba062dc23089ea51a0
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2019-12-18 08:36:46 +0000

    Track the additional and removal of input devices Ignore back button to allow capture of KEYCODE_BACK events

--- app/src/main/java/name/boyle/chris/keytest/KeyTest.java
+++ app/src/main/java/name/boyle/chris/keytest/KeyTest.java
@@ -1,23 +1,41 @@
 package name.boyle.chris.keytest;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.input.InputManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.widget.TextView;
 
-public class KeyTest extends Activity {
+public class KeyTest extends Activity implements InputManager.InputDeviceListener {
 
+	private InputManager mInputManager;
 	TextView text;
 
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.main);
 		text = (TextView) findViewById(R.id.text);
+
+		mInputManager = (InputManager)getSystemService(Context.INPUT_SERVICE);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		writeLog("onResume");
+		mInputManager.registerInputDeviceListener(this, null);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		writeLog("onPause");
+		mInputManager.unregisterInputDeviceListener(this);
 	}
 
 	@Override
@@ -30,6 +48,27 @@ public class KeyTest extends Activity {
 	public boolean onGenericMotionEvent(MotionEvent event) {
 		writeLog("onGenericMotionEvent: " + event.toString());
 		return super.onGenericMotionEvent(event);
+	}
+
+	@Override
+	public void onInputDeviceAdded(int i) {
+		writeLog("onInputDeviceAdded: " + mInputManager.getInputDevice(i));
+	}
+
+	@Override
+	public void onInputDeviceRemoved(int i) {
+		writeLog("onInputDeviceRemoved: " + i);
+	}
+
+	@Override
+	public void onInputDeviceChanged(int i) {
+		writeLog("onInputDeviceChanged: " + i);
+	}
+
+	@Override
+	public void onBackPressed() {
+		writeLog("onBackPressed");
+		// Suppress back button behaviour by not calling overridden method
 	}
 
 	private void writeLog(String logEvent) {
